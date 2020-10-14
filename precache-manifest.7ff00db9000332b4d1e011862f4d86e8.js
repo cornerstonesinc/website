70ccdb32487a0f5190a0c7356845974e4391ddad
newt0n <newt0n@newt0ns-MacBook-Air.local>
2020-10-14 11:02:24 +0530

    Updates

---
+++ precache-manifest.7ff00db9000332b4d1e011862f4d86e8.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2b304d5efcdac1dd77bfe35ad05d828",
+    "url": "/cornerstones_web/index.html"
+  },
+  {
+    "revision": "def4f30d96fe157efa7c",
+    "url": "/cornerstones_web/static/css/2.799926bd.chunk.css"
+  },
+  {
+    "revision": "47b9d9192ba566ab56e4",
+    "url": "/cornerstones_web/static/css/main.7043ed43.chunk.css"
+  },
+  {
+    "revision": "def4f30d96fe157efa7c",
+    "url": "/cornerstones_web/static/js/2.148451a2.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/cornerstones_web/static/js/2.148451a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47b9d9192ba566ab56e4",
+    "url": "/cornerstones_web/static/js/main.7c6f95fd.chunk.js"
+  },
+  {
+    "revision": "4a5f93bd05d9d4c4bfed",
+    "url": "/cornerstones_web/static/js/runtime-main.36f54552.js"
+  },
+  {
+    "revision": "b6598e5e51bf213329fa1ed7f75d36af",
+    "url": "/cornerstones_web/static/media/0.b6598e5e.jpeg"
+  },
+  {
+    "revision": "6e2244d7052747c31a187233e75beaae",
+    "url": "/cornerstones_web/static/media/1.6e2244d7.jpeg"
+  },
+  {
+    "revision": "05ec725b3c2de419ae710d8e3907cc96",
+    "url": "/cornerstones_web/static/media/11.05ec725b.png"
+  },
+  {
+    "revision": "52af892cfafff1c34d416a7b8074f71a",
+    "url": "/cornerstones_web/static/media/12.52af892c.png"
+  },
+  {
+    "revision": "c436cd50ad63e32ac39f19889cf64a36",
+    "url": "/cornerstones_web/static/media/14.c436cd50.png"
+  },
+  {
+    "revision": "eb3a5d122d9e99407e1d494095e1143f",
+    "url": "/cornerstones_web/static/media/15.eb3a5d12.png"
+  },
+  {
+    "revision": "31a02d23ef8faf1adc76ead75ad1ef82",
+    "url": "/cornerstones_web/static/media/16.31a02d23.png"
+  },
+  {
+    "revision": "96bcc11c5cee9bff5faeee01bf2f1b13",
+    "url": "/cornerstones_web/static/media/17.96bcc11c.png"
+  },
+  {
+    "revision": "68daf030148d52e95b85a89e8a3e3bc7",
+    "url": "/cornerstones_web/static/media/18.68daf030.png"
+  },
+  {
+    "revision": "f7a27a83962539b90a7e5d295cdbba70",
+    "url": "/cornerstones_web/static/media/19.f7a27a83.png"
+  },
+  {
+    "revision": "8e436e2a14824e24e17cb5b74b644b25",
+    "url": "/cornerstones_web/static/media/2.8e436e2a.jpeg"
+  },
+  {
+    "revision": "83244cccf9c3a95d7aea730a7742f219",
+    "url": "/cornerstones_web/static/media/20.83244ccc.png"
+  },
+  {
+    "revision": "48649fd745843d9a765c45dd237e90c5",
+    "url": "/cornerstones_web/static/media/21.48649fd7.jpg"
+  },
+  {
+    "revision": "3f65a767cc501fd05638dcb17c710f71",
+    "url": "/cornerstones_web/static/media/3.3f65a767.jpeg"
+  },
+  {
+    "revision": "06a027f1f0808f8132c1b71ffb3fc7ee",
+    "url": "/cornerstones_web/static/media/4.06a027f1.jpeg"
+  },
+  {
+    "revision": "a909317f33518e45745df7cb5c121d95",
+    "url": "/cornerstones_web/static/media/5.a909317f.jpeg"
+  },
+  {
+    "revision": "887b3664aaab8afdeecf24d704704126",
+    "url": "/cornerstones_web/static/media/6.887b3664.jpeg"
+  },
+  {
+    "revision": "3ac45ac3d9d2ecd25ac3d86faf144f17",
+    "url": "/cornerstones_web/static/media/7.3ac45ac3.jpeg"
+  },
+  {
+    "revision": "f92908858a32ff519da27bad914b2a9b",
+    "url": "/cornerstones_web/static/media/8.f9290885.jpeg"
+  },
+  {
+    "revision": "6bf2f102400d36fe26f50bb3e4649320",
+    "url": "/cornerstones_web/static/media/Done.6bf2f102.svg"
+  },
+  {
+    "revision": "d489177bebea158acac91b0e706a0da2",
+    "url": "/cornerstones_web/static/media/Success.d489177b.svg"
+  },
+  {
+    "revision": "d050b5a273a912e43f0d5106e2a1f3c6",
+    "url": "/cornerstones_web/static/media/Waiting.d050b5a2.svg"
+  },
+  {
+    "revision": "d85962184b3236799fce31df036b4fac",
+    "url": "/cornerstones_web/static/media/design_components.d8596218.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/cornerstones_web/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/cornerstones_web/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/cornerstones_web/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/cornerstones_web/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/cornerstones_web/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "784c57d72f384125f8e70d58ab3d8835",
+    "url": "/cornerstones_web/static/media/logoM.784c57d7.png"
+  },
+  {
+    "revision": "9334eaffe1212fc583240e458653e6aa",
+    "url": "/cornerstones_web/static/media/map.9334eaff.svg"
+  },
+  {
+    "revision": "60a81d95bd41f3e102d5ac91d8da8874",
+    "url": "/cornerstones_web/static/media/personal_finance.60a81d95.svg"
+  },
+  {
+    "revision": "e4d22903a7f001e2dde79da81ff8f698",
+    "url": "/cornerstones_web/static/media/respond.e4d22903.svg"
+  },
+  {
+    "revision": "e9132f0ac32a149efd42bd645c1580fd",
+    "url": "/cornerstones_web/static/media/team.e9132f0a.svg"
+  }
+]);
