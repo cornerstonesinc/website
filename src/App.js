75732c0ad163ef0a6d2efc345bf34d06573b6025
newt0n <newt0n@newt0ns-MacBook-Air.local>
2020-10-15 10:32:34 +0530

    hash routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './App.css';
 import Main from './components/MainComponent'
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter, HashRouter} from 'react-router-dom';
 import { ParallaxProvider } from 'react-scroll-parallax';
 
 
 function App() {
   return (
       <ParallaxProvider>
-      <BrowserRouter>
+      <HashRouter>
           <div className="App">
               <Main />
           </div>
-      </BrowserRouter>
+      </HashRouter>
       </ParallaxProvider>
   );
 }
